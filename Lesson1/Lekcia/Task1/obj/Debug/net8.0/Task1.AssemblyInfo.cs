92e8ed6236fe0a009e8bcbeae9a841137b0c87ad
AlexandrZimakov <a-zimakov@mail.ru>
2024-10-06 20:04:59 +0300

    add Lesson1

--- Lesson1/Lekcia/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Lesson1/Lekcia/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5863e5a5902e35febdacc7e7bcc09c5c1909c1b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+591c48595bd2a978386ea9b7912970e2f6d3da57")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

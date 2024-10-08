3c63add8fcb333c7493f60e4992c885466eb248d
AlexandrZimakov <a-zimakov@mail.ru>
2024-10-08 10:49:35 +0300

    firstSem

---
+++ Lesson1/Seminar/01/Program.cs
@@ -0,0 +1,9 @@
+﻿int firstValue = 2;
+int secondValue = 1;
+
+if(firstValue == secondValue * secondValue) {
+    Console.WriteLine($"a = {firstValue}, b = {secondValue} => Да");
+}
+else {
+    Console.WriteLine($"a = {firstValue}, b = {secondValue} => Нет");
+}

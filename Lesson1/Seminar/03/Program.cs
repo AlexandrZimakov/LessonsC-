6bb80802e19d09b76a7a4749fdfd73254e49e97b
AlexandrZimakov <a-zimakov@mail.ru>
2024-10-08 11:43:38 +0300

    Sem03

---
+++ Lesson1/Seminar/03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(99 < number && number < 1000){
+    int leftDigit = number / 100;
+    int rightDigit = number % 10;
+    int result =  leftDigit + rightDigit;
+    System.Console.WriteLine(result);
+}
+else{
+    System.Console.WriteLine("Число не трехзначное");
+}

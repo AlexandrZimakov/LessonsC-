b45c1898c946a6e093ddd66fd496accfa75a0214
AlexandrZimakov <a-zimakov@mail.ru>
2024-10-08 12:06:17 +0300

    Lesson1

--- Lesson1/Lekcia/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Lesson1/Lekcia/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97065ce866e05a3df1ff7916bb13d2467e31b4e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c63add8fcb333c7493f60e4992c885466eb248d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lesson1/Seminar/02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое положительное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//int i = -number;
+//while(i <= number) {
+//    System.Console.Write(i + " ");
+//    i++;
+//}
+
+for(int i = -number; i <= number; i++){
+    System.Console.Write(i + " ");
+}

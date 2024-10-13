1519386ed6573398e7306b6a654c6992cefe3e76
AlexandrZimakov <a-zimakov@mail.ru>
2024-10-13 10:10:04 +0300

    add z1

--- Lesson2/Dz/z1/Program.cs
+++ Lesson2/Dz/z1/Program.cs
@@ -1,3 +1,11 @@
 ﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
 
 
+Console.Write("Напиши число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0) {
+    Console.Write("Кратно");
+} else {
+    Console.Write("Не кратно");
+}

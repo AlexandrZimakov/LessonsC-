ef686f69d212d28f25d9faaf792f1341793bc980
AlexandrZimakov <a-zimakov@mail.ru>
2024-10-13 10:43:22 +0300

    add dz

--- Lesson2/Dz/z3/Program.cs
+++ Lesson2/Dz/z3/Program.cs
@@ -1,1 +1,13 @@
 ﻿//Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Console.Write("Введите число из отрезка [10,99]:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit){
+    Console.WriteLine(firstDigit);
+}else {
+    Console.WriteLine(secondDigit);
+}

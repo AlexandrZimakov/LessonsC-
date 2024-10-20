716c9748e67c4c401c9605fcb0b4530716af4de9
AlexandrZimakov <a-zimakov@mail.ru>
2024-10-20 10:08:29 +0300

    add dz5

---
+++ Lesson5/dz/z2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте двумерный массив целых чисел. Напишите программу, которая
+// находит сумму элементов в каждом столбце массива и выводит её.
+
+class Program
+{
+    static void FindSumInColumns(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int cols = array.GetLength(1);
+        for (int j = 0; j < cols; j++)
+        {
+            int sum = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                sum += array[i, j];
+            }
+            Console.WriteLine($"Сумма элементов в столбце {j}:{sum}");
+        }
+    }
+    static void Main()
+    {
+        int[,] array = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+        FindSumInColumns(array);
+    }
+}

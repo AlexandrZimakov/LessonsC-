fb41e03dee2df04ab468704f7bc6405943a53ac4
AlexandrZimakov <a-zimakov@mail.ru>
2024-10-20 10:38:19 +0300

    add dz6

---
+++ Lesson4/Seminar/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿void Main()
+{
+    int a = 7;
+    int b = 8;
+    int c = 13;
+
+    int d = 10;
+    int e = 5;
+    int f = 3;
+
+    int result = Function(a, b, c);
+    System.Console.WriteLine(result);
+
+    int result2 = Function(d, e, f);
+    System.Console.WriteLine(result2);
+}
+
+int Function(int firstValue, int secondValue, int thirdValue)
+{
+    int result = (firstValue + secondValue - thirdValue) * 2 + 3 * secondValue - 10;
+    return result;
+}
+
+Main();
---
+++ Lesson4/Seminar/task2/Program.cs
@@ -0,0 +1,48 @@
+﻿void Main()
+{
+    int sizeOfArray = ReadInt("Введите размер массива: ");
+    int[] array = GenerateArray(sizeOfArray, 0, 10);
+    PrintArray(array);
+    Console.WriteLine(CountElements(array));
+}
+
+int CountElements(int[] array)
+{
+    int count = 0;
+    foreach (int number in array)
+    {
+        if (number % 10 == 1 && number % 7 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    for (int i = 0; i < arrayForPrint.Length; i++)
+    {
+        System.Console.Write(arrayForPrint[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int[] GenerateArray(int size, int minRange, int maxRange)
+{
+    int[] tempArray = new int[size];
+    Random temp = new Random();
+    for (int i = 0; i < tempArray.Length; i++)
+    {
+        tempArray[i] = temp.Next(minRange , maxRange + 1);
+    }
+    return tempArray;
+}
+
+int ReadInt(string msg)
+{
+    System.Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
